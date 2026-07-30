f650f5e52db794a265bc8f3fe05e3bcbea6d3ff9
Siva <0987944+actuallysiva@users.noreply.github.com>
2026-07-30 08:59:20 +0530

    Move Procfile to backend root

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = os.getenv("DEBUG") == "True"
 
 #ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 ALLOWED_HOSTS = os.environ.get(
