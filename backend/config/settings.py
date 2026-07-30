de219ff37eea7043d2ca3a69f4364be5f0b0fa23
Siva <0987944+actuallysiva@users.noreply.github.com>
2026-07-30 09:47:21 +0530

    Changing allowed_host

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -31,11 +31,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", False) == "True"
 
-#ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
-ALLOWED_HOSTS = os.environ.get(
-    "ALLOWED_HOSTS",
-    ""
-).split(",")
+ALLOWED_HOSTS = ["*"]
+# ALLOWED_HOSTS = os.environ.get(
+#     "ALLOWED_HOSTS",
+#     ""
+# ).split(",")
 
 # Application definition
 
