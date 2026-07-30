e5e0a2f08b7385d4324b6886f4b44a286bc52a06
Siva <0987944+actuallysiva@users.noreply.github.com>
2026-07-30 09:53:41 +0530

    CSRF value

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -31,11 +31,15 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", False) == "True"
 
-ALLOWED_HOSTS = ["*"]
-# ALLOWED_HOSTS = os.environ.get(
-#     "ALLOWED_HOSTS",
-#     ""
-# ).split(",")
+#ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://stepup-production-a990.up.railway.app",
+]
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 # Application definition
 
