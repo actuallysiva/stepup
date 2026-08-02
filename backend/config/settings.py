e4e63f4e50f3899ce32919e3bd6cd8edf96676a7
Siva <0987944+actuallysiva@users.noreply.github.com>
2026-08-02 19:33:26 +0530

    Modified CORS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -166,6 +166,9 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5173',
     'http://localhost:3000',
     'http://127.0.0.1:3000',
+    "https://stepup-three-brown.vercel.app",
+
+
 ]
 
 REST_FRAMEWORK = {
