49b28de7fa2f3443130a58792c3a4551c8c1df74
Siva <0987944+actuallysiva@users.noreply.github.com>
2026-08-02 19:40:52 +0530

    Vercel REgex Added

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -166,9 +166,10 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5173',
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    "https://stepup-three-brown.vercel.app",
+]
 
-
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^https://.*\.vercel\.app$",
 ]
 
 REST_FRAMEWORK = {
