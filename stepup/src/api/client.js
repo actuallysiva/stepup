e425da728e2c6222c8eacc93c5af996285665911
Siva <0987944+actuallysiva@users.noreply.github.com>
2026-08-02 19:22:04 +0530

    Changed for vercel

--- stepup/src/api/client.js
+++ stepup/src/api/client.js
@@ -1,5 +1,5 @@
-const API_BASE = '/api';
-
+//const API_BASE = '/api';
+const API_BASE = import.meta.env.VITE_API_URL;
 async function request(path, options = {}) {
   const { body, headers = {}, ...rest } = options;
 
