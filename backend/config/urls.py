7f1e806c465f47477ecac1c84c7b0a865a8690c6
Siva <0987944+actuallysiva@users.noreply.github.com>
2026-08-12 09:08:04 +0530

    Upload Image 404 Solved

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -18,10 +18,14 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
+from django.views.static import serve
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path("admin/", admin.site.urls),
+    path("api/", include("api.urls")),
+    path(
+        "media/<path:path>",
+        serve,
+        {"document_root": settings.MEDIA_ROOT},
+    ),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
